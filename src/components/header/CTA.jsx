4e6af889b890a653eb9e070d81904e5f0af42e7a
bsarnii <scharnoui@gmail.com>
2023-02-13 11:56:37 +0100

    CTA bugfix

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="#portfolio" download className='btn'>Meine Projekte</a>
+        <a href="#portfolio" className='btn'>Meine Projekte</a>
         <a href="#contact" className='btn btn-primary'>Lass uns sprechen</a>
 
     </div>
