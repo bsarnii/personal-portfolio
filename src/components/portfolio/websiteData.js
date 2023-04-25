af9b500d73654be9604cb259a1a215121f5739e0
bsarnii <scharnoui@gmail.com>
2023-04-25 10:46:26 +0200

    added new website to the portfolio

--- src/components/portfolio/websiteData.js
+++ src/components/portfolio/websiteData.js
@@ -2,13 +2,14 @@ import IMG1 from "../../assets/portfolio-1.png"
 import IMG2 from "../../assets/portfolio-2.png"
 import IMG3 from "../../assets/portfolio-3.png"
 import IMG4 from "../../assets/portfolio-4.png"
+import IMG5 from "../../assets/portfolio-5.png"
 
 export const data = [
     {
         id: 1,
         image: IMG1,
         title: "Easybank",
-        tech: "HTML, CSS, Angular, ngrx, Adzuna API",
+        tech: "HTML, CSS, TypeScript, Angular, ngrx, Adzuna API",
         github: "https://github.com/bsarnii/easybank",
         demo: "https://beamish-duckanoo-11e2a8.netlify.app/",
         description: "Das Easybank-Projekt ist ein Beispiel für eine gut strukturierte und reaktive Angular-Anwendung, die mithilfe von Angular Router und ngrx eine intuitive Navigation und eine zuverlässige Datenflussverwaltung ermöglicht. Es verwendet Angular Reactive Forms, um ein einfaches und leistungsstarkes Formularsystem zu implementieren."
@@ -16,6 +17,15 @@ export const data = [
     {
         id: 2,
         image: IMG2,
+        title: "Task management app",
+        tech: "HTML, SCSS, TypeScript, Angular",
+        github: "https://github.com/bsarnii/kanban-task-manager",
+        demo: "https://famous-sopapillas-6eda62.netlify.app/",
+        description: "Diese Anwendung ermöglicht es Benutzern, verschiedene Tafeln und Aufgaben zu erstellen, zu bearbeiten oder zu löschen. Man kann die Aufgaben mit Drag & Drop funktion zwischen die Spalten verschieben. Eine weitere Funktion meine Anwendung ist die Verwendung des Browser localstorage, um alle veränderten Daten zu speichern. Durch diese Funktion können Benutzer sicher sein, dass ihre Daten auch dann nicht verloren gehen, wenn den Browser schließen. Es gibt auch die Möglichkeit, das Farbthema der gesamten App zu ändern."
+    },
+    {
+        id: 3,
+        image: IMG3,
         title: "Interactive comment section",
         tech: "HTML, SCSS, TypeScript, React, redux-toolkit, Firebase",
         github: "https://github.com/bsarnii/interactive-comment-section",
@@ -23,8 +33,8 @@ export const data = [
         description: "Das Projekt ist eine React-Webanwendung, die mit Typescript entwickelt wurde und Firebase als Backend nutzt. Es ermöglicht den Benutzern, sich mit Google-Authentifizierung anzumelden und neue Kommentare zu schreiben, zu bearbeiten, zu löschen oder auf andere Kommentare zu antworten."
     },
     {
-        id: 3,
-        image: IMG3,
+        id: 4,
+        image: IMG4,
         title: "Weather app",
         tech: "HTML, CSS, JavaScript, React, Weather API",
         github: "https://github.com/bsarnii/weather-app",
@@ -32,8 +42,8 @@ export const data = [
         description: "Das Weather App ist eine React-Webanwendung, die es dem Benutzer ermöglicht, die Wetterdaten von verschiedenen Städten abzurufen. Durch die Verbindung mit einer Wetter-API können die aktuellen Wetterbedingungen für jede Stadt angezeigt werden, einschließlich der Temperatur sowie der Vorhersage für die kommenden Tage."
     },
     {
-        id: 4,
-        image: IMG4,
+        id: 5,
+        image: IMG5,
         title: "IP-Adresse / Domain finder",
         tech: "HTML, CSS, JavaScript, React, IP Geolocation API, react-leaflet", 
         github: "https://github.com/bsarnii/ip-adress-tracker",
